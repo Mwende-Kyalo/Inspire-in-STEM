6f036fe7dafd1197758714632558d1b16d35f462
Mwende-Kyalo <traceyndanukyalo@gmail.com>
2022-06-02 11:01:10 +0300

    classes

---
+++ classes/person.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python 
+#################################### 
+#    Name : Mwende
+#    Date :02/ 06/ 2022 
+##################################### 
+
+class Person:
+    def __init__(self, _name, _age):
+        self.name= _name
+        self.age= _age
+
+    def sayHi(self):
+        print('Hello, my name is ' + self.name + ' and i am ' + str(self.age)+ ' years old')
+#person1
+person1 = Person('Bob', 16 )
+person1.sayHi()
+
+#person2
+person2 =Person('Angie',18)
+person2.sayHi()
+
+

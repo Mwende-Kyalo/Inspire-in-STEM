86ec9a57ac38575c67e79d02b4be8563549b089e
Mwende-Kyalo <traceyndanukyalo@gmail.com>
2022-06-06 11:08:46 +0300

    geometric progression

---
+++ geometricprogression.py
@@ -0,0 +1,21 @@
+#Geometric progression
+#!/usr/bin/python 
+#################################### 
+#    Name : Mwende
+#    Date :06/ 06/ 2022 
+##################################### 
+
+a = int(input("Enter the first term : "))
+r = float(input("Enter the common ratio : "))
+n = int(input("Enter the number : "))
+
+for i in range(1,n+1):
+    n_term = a*r**(n-1) 
+print(n_term)
+
+for float in range (1,n+1):
+    if r > 1 :
+        sum_nums = (a -(r **(n - 1) -1)) / (r-1)
+    elif r < 1 :
+        sum_nums = (a - (1 -(r ** (n - 1)))) / (1 - r)
+print(sum_nums)
